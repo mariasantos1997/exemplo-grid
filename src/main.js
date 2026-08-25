7b3a51046629a6c3f97dbd8e0bc64acbace6c433
Maria Eduarda Alves Dos Santos <marydogenshin@gmail.com>
2026-08-25 20:53:09 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+// Importa TODO o CSS do Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importa estilos personalizados da aplicação
 import './style.css';
 
 console.log('Bootstrap e vite carregados com sucesso');
